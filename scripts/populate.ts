5e7cae0b8add8b4f3cba9713d3d2c7bc09944f03
WeiJensen <weijensen0106@gmail.com>
2024-02-07 14:10:57 +0100

    chore(): re-add populate script

--- scripts/populate.ts
+++ scripts/populate.ts
@@ -1,37 +1,74 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const db = new PrismaClient();
+const db = new PrismaClient()
 
 const main = async () => {
-    // create data for the database, { date: Date, value: float }. 100 Records 100 days back to today
+    const length = await db.data.count()
 
-    const data = Array.from({ length: 100 }, (_, i) => {
-        const date = new Date();
-        date.setDate(date.getDate() - i);
-        return {
-            date,
-            value: Math.random() * 100,
-        };
-    }).sort((a, b) => a.date.getTime() - b.date.getTime());
+    if (length === 0) {
+        console.log('Populating data...')
 
-    await db.data.createMany({
-        data,
-    });
+        const data = Array.from({ length: 100 }, (_, i) => {
+            const date = new Date()
+            date.setDate(date.getDate() - i)
+            return {
+                date,
+                value: Math.random() * 100,
+            }
+        }).sort((a, b) => a.date.getTime() - b.date.getTime())
 
-    const allData = await db.data.findMany();
-    console.table({
-        length: allData.length,
-        first: allData[0],
-        last: allData[allData.length - 1],
-    });
+        await db.data.createMany({
+            data,
+        })
+
+        const allData = await db.data.findMany()
+        console.table({
+            length: allData.length,
+            first: allData[0],
+            last: allData[allData.length - 1],
+        })
+    } else {
+        console.log('Data already exists. Adding new data...')
+        const lastDate = (await db.data.findFirst({
+            orderBy: {
+                date: 'desc',
+            },
+        })) ?? { date: new Date() }
+        const today = new Date()
+
+        const data = Array.from(
+            {
+                length:
+                    (today.getTime() - lastDate.date.getTime()) /
+                    (1000 * 60 * 60 * 24),
+            },
+            (_, i) => {
+                const date = new Date()
+                date.setDate(lastDate.date.getDate() + i)
+                return {
+                    date,
+                    value: Math.random() * 100,
+                }
+            },
+        )
+
+        await db.data.createMany({
+            data,
+        })
+
+        const allData = await db.data.findMany()
+        console.table({
+            length: allData.length,
+            first: allData[0],
+            last: allData[allData.length - 1],
+        })
+    }
 }
 
 main()
-    .catch(e => {
-        throw e;
+    .catch((e) => {
+        throw e
     })
     .finally(async () => {
-        await db.$disconnect();
-    });
-
-
+        await db.$disconnect()
+    })
