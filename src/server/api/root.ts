96a71ea108f3b7ea09194d508f197eae56c6f31b
WeiJensen <weijensen0106@gmail.com>
2024-02-07 10:32:36 +0100

    Initial commit

---
+++ src/server/api/root.ts
@@ -0,0 +1,8 @@
+import { dataRouter } from "~/server/api/routers/data";
+import { createTRPCRouter } from "~/server/api/trpc";
+
+export const appRouter = createTRPCRouter({
+  data: dataRouter,
+});
+
+export type AppRouter = typeof appRouter;
