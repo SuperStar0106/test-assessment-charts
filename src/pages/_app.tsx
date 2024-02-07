563181952fc70569cb56fe5d2071f1f898174788
WeiJensen <weijensen0106@gmail.com>
2024-02-07 11:10:51 +0100

    Add next-themes package and implement dark theme support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,14 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ThemeProvider } from "~/components/ThemeProvider";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="dark">
+    <Component {...pageProps} />;
+    </ThemeProvider>
+  )
 };
 
 export default api.withTRPC(MyApp);
--- src/server/api/routers/data.ts
+++ src/server/api/routers/data.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const dataRouter = createTRPCRouter({
-  
+    
 });
